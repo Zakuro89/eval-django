7a631bae7c4cea690414dff82839d9c2a62f58f0
a.franclet <a.franclet@jeannin-automobiles.com>
2025-05-05 12:15:31 +0200

    commit securisation event_details

--- eventManager/eventManagerApp/views.py
+++ eventManager/eventManagerApp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import T_Event, T_Participation
 from django.db.models import Count
 from django.contrib.auth import authenticate, login as auth_login
-
+from django.contrib.auth.decorators import login_required
 
 def login(request):
   error = None
@@ -27,14 +27,18 @@ def events_list(request):
    
   return render(request, 'events-list.html', {'events': events, 'user': request.user})
   
+@login_required
 def event_details(request, event_id):
    
   event = get_object_or_404(T_Event, id=event_id)
   event.nb_participants = event.participations.count()
 
+  if request.method == 'POST' and request.user.is_authenticated:
+    T_Participation.objects.get_or_create(user_id=request.user, event_id=event)
   
   return render(request, 'event-details.html', {
     'event': event,
     'user': request.user
   })
+
        
